e85d1bc2df4a6561e9f9075fda126e41c852e720
TomRepo <tomkornfeld@gmail.com>
2022-11-27 17:08:26 +0200

    try to fail

--- helloRefael2.js
+++ helloRefael2.js
@@ -2,9 +2,4 @@ console.log("my name is dotan ");
 console.log("no no no ");
 console.log("my name is Refael ");
 console.log("yes yes yes yes!");
-console.log("120 ");
 
-console.log("120 ");
-
-
-console.log("130 ");
