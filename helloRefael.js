cf3665ebf5053b0683d4de272707679200fadbcf
refael.sitbon <refael.sitbon@gmail.com>
2022-11-27 16:01:43 +0200

    dsf

---
+++ helloRefael.js
@@ -0,0 +1,6 @@
+console.log("my name is Refael sitbon ");
+
+console.log("856485648564 is my favorite num");
+const date = new Date();
+console.log("The time is - " + date);
+
--- helloTom.js
+++
@@ -1,6 +0,0 @@
-console.log("my name is Tom Kornfeld ");
-
-console.log("23 is my favorite num");
-const date = new Date();
-console.log("The time is - " + date);
-
